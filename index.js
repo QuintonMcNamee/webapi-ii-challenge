8b5506e3a2c35c1a43c7f56cfd41a66f519bdebf
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-09-05 18:45:59 -0400

    dynamic

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // implement your API here
 
+require("dotenv").config();
+
 const express = require('express');
 
 const db = require('./data/db.js');
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const server = express();
 server.use(express.json());
